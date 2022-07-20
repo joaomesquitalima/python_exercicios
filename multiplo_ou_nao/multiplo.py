e4a411f68ea3f226c90f88069cce0521d1e4aba7
João Vitor Mesquita de Lima <99664194+joaomesquitalima@users.noreply.github.com>
2022-07-20 10:17:04 -0400

    Create multiplo.py

---
+++ multiplo_ou_nao/multiplo.py
@@ -0,0 +1,8 @@
+m = float(input('Digite um numero qualquer :'))
+n = float(input('Digite um numero multiplo :'))
+numero = m/n
+if(numero // 1 == numero): 
+    print('\nÉ multiplo !')
+else:
+    print('\nNão é multiplo !')
+    
